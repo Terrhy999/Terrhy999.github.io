ed6882fc75fa23b58e751bf9bd06c0b51ce58b27
T Hyland <thyla999@gmail.com>
2020-09-14 14:26:33 -0400

    Menu

---
+++ script.js
@@ -0,0 +1,13 @@
+// const websiteWord = document.getElementById("websiteWord");
+ 
+// websiteWord.addEventListener("mouseover", setBlue, false);
+
+// websiteWord.addEventListener("mouseleave", setPink, false);
+ 
+// function setBlue() {
+//     websiteWord.style.cssText = "color: blue;";
+// }
+
+//  function setPink() {
+//      websiteWord.style.cssText = "color: palevioletred;";
+//  }
